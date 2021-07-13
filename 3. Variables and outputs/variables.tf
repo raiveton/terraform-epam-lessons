5497dc4c715eff9db0793d26df6f15db623806c1
raiveton <56071129+raiveton@users.noreply.github.com>
2021-07-13 07:01:45 +0600

    Update variables.tf

--- 3. Variables and outputs/variables.tf
+++ 3. Variables and outputs/variables.tf
@@ -1,12 +1,12 @@
 variable "ami" {
  default = "ami-0a16233627f2c1a13"
  type = string
- description = ami for instance
+ description = "ami for instance"
 }
 variable "instance_type" {
  default = "t3.micro" 
 } 
-variable "count" {
+variable "counts" {
  default = 1
  type = number   
 }
